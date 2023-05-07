5bb6f076746e3ff64dd9908680d4e0c3d0f5fbd7
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-05-06 17:42:31 -0700

    added cities

---
+++ server.js
@@ -0,0 +1,46 @@
+
+'use strict';
+//this package reads env files,config methos is to process the file and adds them to the process.evc obj
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const weatherData = require('./data.json');
+//initialize express
+const app = express();
+//middleware to allow open access with cors. It will be executed for each incoming request to the application
+app.use(cors());
+const PORT = process.env.PORT;
+app.get('/', (req,res)=>(
+  res.status(200).send('Default route is working')
+));
+//http://localhost:3001/weatherData
+app.get('/weatherData',(req,res)=>{
+  const {searchQuery} = req.query;
+  const cityData = weatherData.find(city=>city.city_name===searchQuery)
+  const formattedData = cityData.data.map(obj=>{
+    return new Forecast(obj);
+  });
+
+  res.status(200).send(formattedData);
+  // if (searchQuery === 'Seattle'){
+  //   const formattedData = weatherData[0].data.map(obj=>{
+  //     return new Forecast(obj);
+  //   });
+  //   console.log(formattedData);
+  //   res.status(200).send(formattedData);
+  // }
+  // else if (searchQuery === 'Paris'){
+  //   res.status(200).send(weatherData[1]);
+  // }
+  // else if (searchQuery === 'Amman'){
+  //   res.status(200).send(weatherData[2]);
+  //   console.log(weatherData[2].city_name);
+  // }
+});
+class Forecast {
+  constructor(obj){
+    this.date = obj.valid_date;
+    this.description=obj.weather.description;
+  }
+}
+app.listen(PORT, ()=>console.log(`listening on ${PORT}`));

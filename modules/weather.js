41ccafc52fdf4ab5860b8da84ce6facaa5bd349c
ZG79 <101478282+ZG79@users.noreply.github.com>
2023-05-18 13:31:10 -0700

    added cache code

--- modules/weather.js
+++ modules/weather.js
@@ -1,14 +1,26 @@
 'use strict';
 
 const axios = require('axios');
+let cache = require('./cache');
 
 function getWeather(req, res, next) {
+  const key = 'weather-' + lat + lon;
   const { lat, lon } = req.query;
   const urlApi = `http://api.weatherbit.io/v2.0/forecast/daily/?lat=${lat}&key=${process.env.WEATHER_API_KEY}&lon=${lon}&days=5`;
-  axios.get(urlApi)
-    .then(response => response.data.data.map(element => new MyWeather(element)))
-    .then(data => res.status(200).send(data))
-    .catch(err => next(err));
+
+  if (cache[key]&&(Date.now() - cache[key].timestamp<50000)){
+    console.log('Cache hit');
+  }else{
+    console.log('Cache miss');
+    cache[key]={};
+    cache[key].timestamp = Date.now();
+    cache[key].data = 
+    axios.get(urlApi)
+      .then(response => response.data.data.map(element => new MyWeather(element)))
+    //add local json instead of API (8-12)
+      .then(data => res.status(200).send(data))
+      .catch(err => next(err));
+  }
 }
 
 class MyWeather {
